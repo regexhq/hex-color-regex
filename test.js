678d2ae8e8a341907628e6dcf18df53ef4959fad
tunnckoCore <mameto_100@mail.bg>
2015-02-03 01:35:12 +0200

    update tests

--- test.js
+++ test.js
@@ -23,15 +23,33 @@ var sixDigits = {
     '#7B68EE',
     '#eeeeee',
     '#ffffff',
-    '#111111'
+    '#111111',
+    // '#123fff}' // should pass
   ],
   fail: [
-    '#afebe',
-    '#3cb37',
+    'afebe3',
+    'AFEBE3',
+    '3cb371',
+    'ABC371',
+    '556b2f',
+    '5A6B2F',
+    '708090',
+    '7b68ee',
+    '7B68EE',
+    'eeeeee',
+    'ffffff',
+    '111111',
+    'afebef',
+    '3c537f',
     '556B2f',
     '708135',
-    '#ZE3EF1',
-    '#7g68ZY'
+    'EE3EF1',
+    '7f68ZY',
+    '#7f68ZY',
+    '#7z68ZY',
+    '#GR68',
+    '#Z68',
+    '#666EFR'
   ]
 };
 var threeDigits = {
@@ -48,23 +66,29 @@ var threeDigits = {
     '#777',
     '#FFF',
     '#fff',
+    // '#f3f}', should pass
     '#111'
   ],
   fail: [
-    '#afg',
-    '#4zy',
+    'fff',
+    '4zy',
     '4g1',
     '111',
+    'Ge3',
+    'zY1',
+    '#ggg',
+    '#4zy',
+    '#4g1',
     '#Ge3',
     '#zY1'
   ]
 };
 
 sixDigits.pass.forEach(function(hex) {
-  mukla('should be `true` when `'+ hex +'` hex value').strictEqual(regex().test(hex), true);
+  mukla('should be `true` when `'+ hex +'` value').strictEqual(regex().test(hex), true);
 });
 sixDigits.fail.forEach(function(hex) {
-  mukla('should be `false` when `'+ hex +'` hex value').strictEqual(regex().test(hex), false);
+  mukla('should be `false` when `'+ hex +'` value').strictEqual(regex().test(hex), false);
 });
 
 threeDigits.pass.forEach(function(hex) {
