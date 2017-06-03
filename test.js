07d1725248b4a38440bc4d32aa848766f99a105a
tunnckoCore <mameto2011@gmail.com>
2017-06-03 04:27:14 +0300

    use mukla for testing, reformat test.js

--- test.js
+++ test.js
@@ -9,7 +9,7 @@
 
 'use strict'
 
-var test = require('assertit')
+var test = require('mukla')
 var hexColorRegex = require('./index')
 
 var sixDigits = {
@@ -88,174 +88,149 @@ var threeDigits = {
 }
 
 var fourDigits = {
-  pass: [
-    '#afe0',
-    '#AF31',
-    '#3cba',
-    '#3CBA',
-    '#b2ff',
-    '#5B2F'
-  ],
-  fail: [
-    'afe0',
-    'AF31',
-    '#3cbg',
-    '#3CBy',
-    '#b2fz'
-  ]
+  pass: ['#afe0', '#AF31', '#3cba', '#3CBA', '#b2ff', '#5B2F'],
+  fail: ['afe0', 'AF31', '#3cbg', '#3CBy', '#b2fz']
 }
 
 var eightDigits = {
-  pass: [
-    '#afebe300',
-    '#AFEBE3AA',
-    '#3cb371ff',
-    '#3CB371CC',
-    '#556b2f55'
-  ],
-  fail: [
-    'afebe300',
-    'AFEBE3AA',
-    '#3cb371fg',
-    '#3CB371xy',
-    '#556b2fz9'
-  ]
+  pass: ['#afebe300', '#AFEBE3AA', '#3cb371ff', '#3CB371CC', '#556b2f55'],
+  fail: ['afebe300', 'AFEBE3AA', '#3cb371fg', '#3CB371xy', '#556b2fz9']
 }
 
-test('hex-color-regex:', function () {
-  test('in no strict mode', function () {
-    test('six digit hex', function () {
-      test('should be `true`', function () {
-        sixDigits.pass.forEach(function (hex) {
-          test('when `' + hex + '` value', function () {
+test('hex-color-regex:', function() {
+  test('in no strict mode', function() {
+    test('six digit hex', function() {
+      test('should be `true`', function() {
+        sixDigits.pass.forEach(function(hex) {
+          test('when `' + hex + '` value', function() {
             test.equal(hexColorRegex().test(hex), true)
           })
         })
-        test('when `foo #ae3f4c bar` value', function () {
+        test('when `foo #ae3f4c bar` value', function() {
           test.equal(hexColorRegex().test('foo #ae3f4c bar'), true)
         })
       })
-      test('should be `false`', function () {
-        sixDigits.fail.forEach(function (hex) {
-          test('when `' + hex + '` value', function () {
+      test('should be `false`', function() {
+        sixDigits.fail.forEach(function(hex) {
+          test('when `' + hex + '` value', function() {
             test.equal(hexColorRegex().test(hex), false)
           })
         })
       })
     })
-    test('three digit hex', function () {
-      test('should be `true`', function () {
-        threeDigits.pass.forEach(function (hex) {
-          test('when `' + hex + '` value', function () {
+    test('three digit hex', function() {
+      test('should be `true`', function() {
+        threeDigits.pass.forEach(function(hex) {
+          test('when `' + hex + '` value', function() {
             test.equal(hexColorRegex().test(hex), true)
           })
         })
-        test('when `foo #e4f bar` value', function () {
+        test('when `foo #e4f bar` value', function() {
           test.equal(hexColorRegex().test('foo #e4f bar'), true)
         })
       })
-      test('should be `false`', function () {
-        threeDigits.fail.forEach(function (hex) {
-          test('when `' + hex + '` value', function () {
+      test('should be `false`', function() {
+        threeDigits.fail.forEach(function(hex) {
+          test('when `' + hex + '` value', function() {
             test.equal(hexColorRegex().test(hex), false)
           })
         })
       })
     })
-    test('eight digit alpha channel hex', function () {
-      test('should be `true`', function () {
-        eightDigits.pass.forEach(function (hex) {
-          test('when `' + hex + '` value', function () {
+    test('eight digit alpha channel hex', function() {
+      test('should be `true`', function() {
+        eightDigits.pass.forEach(function(hex) {
+          test('when `' + hex + '` value', function() {
             test.equal(hexColorRegex().test(hex), true)
           })
         })
-        test('when `foo #ae3f4c bar` value', function () {
+        test('when `foo #ae3f4c bar` value', function() {
           test.equal(hexColorRegex().test('foo #ae3f4c00 bar'), true)
         })
       })
-      test('should be `false`', function () {
-        eightDigits.fail.forEach(function (hex) {
-          test('when `' + hex + '` value', function () {
+      test('should be `false`', function() {
+        eightDigits.fail.forEach(function(hex) {
+          test('when `' + hex + '` value', function() {
             test.equal(hexColorRegex().test(hex), false)
           })
         })
       })
     })
-    test('four digit alpha channel hex', function () {
-      test('should be `true`', function () {
-        fourDigits.pass.forEach(function (hex) {
-          test('when `' + hex + '` value', function () {
+    test('four digit alpha channel hex', function() {
+      test('should be `true`', function() {
+        fourDigits.pass.forEach(function(hex) {
+          test('when `' + hex + '` value', function() {
             test.equal(hexColorRegex().test(hex), true)
           })
         })
-        test('when `foo #ae3f4c bar` value', function () {
+        test('when `foo #ae3f4c bar` value', function() {
           test.equal(hexColorRegex().test('foo #ae3f bar'), true)
         })
       })
-      test('should be `false`', function () {
-        fourDigits.fail.forEach(function (hex) {
-          test('when `' + hex + '` value', function () {
+      test('should be `false`', function() {
+        fourDigits.fail.forEach(function(hex) {
+          test('when `' + hex + '` value', function() {
             test.equal(hexColorRegex().test(hex), false)
           })
         })
       })
     })
-    test('using regex().exec(hex)', function () {
-      sixDigits.pass.forEach(function (hex) {
+    test('using regex().exec(hex)', function() {
+      sixDigits.pass.forEach(function(hex) {
         var hexed = hex.replace('}', '')
-        test('should match `' + hexed + '` when `' + hex + '` hex', function () {
+        test('should match `' + hexed + '` when `' + hex + '` hex', function() {
           var actual = hexColorRegex().exec(hex)[0]
           var expected = hexed
 
           test.equal(actual, expected)
         })
       })
-      test('should match `#ae3f4c` when `foo #ae3f4c bar` string', function () {
+      test('should match `#ae3f4c` when `foo #ae3f4c bar` string', function() {
         var actual = hexColorRegex().exec('foo #ae3f4c bar')[0]
         var expected = '#ae3f4c'
 
         test.equal(actual, expected)
       })
-      threeDigits.pass.forEach(function (hex) {
+      threeDigits.pass.forEach(function(hex) {
         var hexed = hex.replace('}', '')
-        test('should match `' + hexed + '` when `' + hex + '` hex', function () {
+        test('should match `' + hexed + '` when `' + hex + '` hex', function() {
           var actual = hexColorRegex().exec(hex)[0]
           var expected = hexed
 
           test.equal(actual, expected)
         })
       })
-      test('should match `#e7f` when `foo #e7f bar` string', function () {
+      test('should match `#e7f` when `foo #e7f bar` string', function() {
         var actual = hexColorRegex().exec('foo #e7f bar')[0]
         var expected = '#e7f'
 
         test.equal(actual, expected)
       })
-      eightDigits.pass.forEach(function (hex) {
+      eightDigits.pass.forEach(function(hex) {
         var hexed = hex.replace('}', '')
-        test('should match `' + hexed + '` when `' + hex + '` hex', function () {
+        test('should match `' + hexed + '` when `' + hex + '` hex', function() {
           var actual = hexColorRegex().exec(hex)[0]
           var expected = hexed
 
           test.equal(actual, expected)
         })
       })
-      test('should match `#ae3f4c00` when `foo #ae3f4c00 bar` string', function () {
+      test('should match `#ae3f4c00` when `foo #ae3f4c00 bar` string', function() {
         var actual = hexColorRegex().exec('foo #ae3f4c00 bar')[0]
         var expected = '#ae3f4c00'
 
         test.equal(actual, expected)
       })
-      fourDigits.pass.forEach(function (hex) {
+      fourDigits.pass.forEach(function(hex) {
         var hexed = hex.replace('}', '')
-        test('should match `' + hexed + '` when `' + hex + '` hex', function () {
+        test('should match `' + hexed + '` when `' + hex + '` hex', function() {
           var actual = hexColorRegex().exec(hex)[0]
           var expected = hexed
 
           test.equal(actual, expected)
         })
       })
-      test('should match `#e7f0` when `foo #e7f0 bar` string', function () {
+      test('should match `#e7f0` when `foo #e7f0 bar` string', function() {
         var actual = hexColorRegex().exec('foo #e7f0 bar')[0]
         var expected = '#e7f0'
 
@@ -263,51 +238,54 @@ test('hex-color-regex:', function () {
       })
     })
   })
-  test('in strict mode', function () {
-    test('six digit hex `#123fff}` should return false', function () {
-      test.equal(hexColorRegex({strict: true}).test('#123fff}'), false)
+  test('in strict mode', function() {
+    test('six digit hex `#123fff}` should return false', function() {
+      test.equal(hexColorRegex({ strict: true }).test('#123fff}'), false)
     })
-    test('string contain six digit hex `foo #ae3f4c bar` return false', function () {
-      test.equal(hexColorRegex({strict: true}).test('foo #ae3f4c bar'), false)
+    test('string contain six digit hex `foo #ae3f4c bar` return false', function() {
+      test.equal(hexColorRegex({ strict: true }).test('foo #ae3f4c bar'), false)
     })
-    test('three digit hex `#f3f}` should return false', function () {
-      test.equal(hexColorRegex({strict: true}).test('#f3f}'), false)
+    test('three digit hex `#f3f}` should return false', function() {
+      test.equal(hexColorRegex({ strict: true }).test('#f3f}'), false)
     })
-    test('string contain three digit hex `foo #e7f bar` return false', function () {
-      test.equal(hexColorRegex({strict: true}).test('foo #e7f bar'), false)
+    test('string contain three digit hex `foo #e7f bar` return false', function() {
+      test.equal(hexColorRegex({ strict: true }).test('foo #e7f bar'), false)
     })
-    test('eight digit alpha channel hex `#123fff00}` should return false', function () {
-      test.equal(hexColorRegex({strict: true}).test('#123fff00}'), false)
+    test('eight digit alpha channel hex `#123fff00}` should return false', function() {
+      test.equal(hexColorRegex({ strict: true }).test('#123fff00}'), false)
     })
-    test('string contain eight digit alpha channel hex `foo #ae3f4cff bar` return false', function () {
-      test.equal(hexColorRegex({strict: true}).test('foo #ae3f4cff bar'), false)
+    test('string contain eight digit alpha channel hex `foo #ae3f4cff bar` return false', function() {
+      test.equal(
+        hexColorRegex({ strict: true }).test('foo #ae3f4cff bar'),
+        false
+      )
     })
-    test('four digit alpha channel hex `#f3f0}` should return false', function () {
-      test.equal(hexColorRegex({strict: true}).test('#f3f0}'), false)
+    test('four digit alpha channel hex `#f3f0}` should return false', function() {
+      test.equal(hexColorRegex({ strict: true }).test('#f3f0}'), false)
     })
-    test('string contain four digit alpha channel hex `foo #e7ff bar` return false', function () {
-      test.equal(hexColorRegex({strict: true}).test('foo #e7ff bar'), false)
+    test('string contain four digit alpha channel hex `foo #e7ff bar` return false', function() {
+      test.equal(hexColorRegex({ strict: true }).test('foo #e7ff bar'), false)
     })
-    test('should not match when `foo #ae3f4c bar` string', function () {
-      var actual = hexColorRegex({strict: true}).exec('foo #ae3f4c bar')
+    test('should not match when `foo #ae3f4c bar` string', function() {
+      var actual = hexColorRegex({ strict: true }).exec('foo #ae3f4c bar')
       var expected = null
 
       test.equal(actual, expected)
     })
-    test('should not match when `foo #e7f bar` string', function () {
-      var actual = hexColorRegex({strict: true}).exec('foo #e7f bar')
+    test('should not match when `foo #e7f bar` string', function() {
+      var actual = hexColorRegex({ strict: true }).exec('foo #e7f bar')
       var expected = null
 
       test.equal(actual, expected)
     })
-    test('should not match when `foo #ae3f4cff bar` string', function () {
-      var actual = hexColorRegex({strict: true}).exec('foo #ae3f4cff bar')
+    test('should not match when `foo #ae3f4cff bar` string', function() {
+      var actual = hexColorRegex({ strict: true }).exec('foo #ae3f4cff bar')
       var expected = null
 
       test.equal(actual, expected)
     })
-    test('should not match when `foo #e7ff bar` string', function () {
-      var actual = hexColorRegex({strict: true}).exec('foo #e7ff bar')
+    test('should not match when `foo #e7ff bar` string', function() {
+      var actual = hexColorRegex({ strict: true }).exec('foo #e7ff bar')
       var expected = null
 
       test.equal(actual, expected)
